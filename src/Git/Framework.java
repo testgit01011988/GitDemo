c576a6d750956f4ec26d4b20878d8186d4fec259
Ash <testgit01011988>
2025-03-07 20:01:33 +0530

    This commit was made by Architect B

--- src/Git/Framework.java
+++ src/Git/Framework.java
@@ -5,6 +5,7 @@ public class Framework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("class made for framework changes by Architect A");
+System.out.println("class made for framework changes by Architect B");
 	}
 
 }
