7ad9ef9d06ccd8937fd70b9d3879cf3dca0057d1
Ash <testgit01011988>
2025-03-07 17:46:37 +0530

    This commit is by user X

--- src/Git/Hello.java
+++ src/Git/Hello.java
@@ -6,7 +6,9 @@ public class Hello {
 		// TODO Auto-generated method stub
 		
 		System.out.println("this is original line 1");
-		System.out,println("This is update done by person B");
+		System.out.println("This is update done by person Y"); //corrected the error by user X
+		System.out.println("This is new update done by person Z"); 
+		
 	}
 
 }
